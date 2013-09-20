9784275eb1847024b50d8e96b869b83886765250
the-ms <the-ms@ya.ru>
2013-09-20 10:04:29 +0700

    add number formatter

--- www/lib/js/extend.js
+++ www/lib/js/extend.js
@@ -31,6 +31,36 @@ Math.rand = function(min, max) {
 };
 
 /**
+ * Format a number with grouped thousands
+ * @param {Number} decimals
+ * @param {String} dec_point
+ * @param {String} thousands_sep
+ * @return {String} A formatted version of number
+ */
+String.prototype.numberFormat = function ( decimals, dec_point, thousands_sep) {
+	var number = (this + '').replace(/[^0-9+\-Ee.]/g, ''),
+		n = !isFinite(+number) ? 0 : +number,
+		prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+		sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+		dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+		s = '',
+		toFixedFix = function (n, prec) {
+			var k = Math.pow(10, prec);
+			return '' + Math.round(n * k) / k;
+		};
+	s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+	if (s[0].length > 3) {
+		s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	}
+	if ((s[1] || '').length < prec) {
+		s[1] = s[1] || '';
+		s[1] += new Array(prec - s[1].length + 1).join('0');
+	}
+	return s.join(dec);
+};
+Number.prototype.numberFormat = String.prototype.numberFormat;
+
+/**
  * Return cookie value by name
  * @param {String} name
  * @return {String|Null} value
