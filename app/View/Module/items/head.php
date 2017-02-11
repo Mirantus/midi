8367713d1a4ca52c8e48240a1e6111b6a18fcdfd
the-ms <the-ms@ya.ru>
2017-02-11 18:23:46 +0700

    правильное подключение ресурсов

--- app/View/Module/items/head.php
+++ app/View/Module/items/head.php
@@ -6,3 +6,4 @@
 <title><?=$this->title?> - <?=$this->app->title;?></title>
 <meta name="description" content="Модуль">
 <link rel="stylesheet" href="/i/icons/icons.css?<?=$this->app->version?>" type="text/css">
+<link rel="stylesheet" href="/js/jquery-sortable/jquery.sortable.css" type="text/css">
